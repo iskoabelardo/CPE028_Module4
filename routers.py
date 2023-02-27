a49b27963e4f22973e1e9c659e377787c4096844
iskoabelardo <qafgabelardo@tip.edu.ph>
2023-02-27 08:52:13 +0000

    try

---
+++ routers.py
@@ -0,0 +1,18 @@
+
+
+#Abelardo, Angelo Francis
+
+routers = ["R1","R2", "R3"]
+
+ipAddress = ['10.0.0.1', '10.0.0.2', '10.0.0.3']
+
+print(routers[0] + ": " + (ipAddress[0]))
+print(routers[1] + ": " + (ipAddress[1]))
+print(routers[2] + ": " + (ipAddress[2]))
+
+
+#routers = {["R1": 10.0.0.1], 
+           #["R2": 10.0.0.2],
+           #["R3": 10.0.0.3]}
+
+#print(ipAddress)
